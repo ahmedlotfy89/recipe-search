6b4c71b711213d7729fcf05eeca1213940c4d72b
DESKTOP-QB0S9KG\Pc <eng.ahmedlotfy89@gmail.com>
2024-05-02 18:22:04 -0700

    Add filters and search bar

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 import { Recipe } from "../models/Recipe";
 import api from "../api/api";
 import { catchError, of, take } from "rxjs";
@@ -7,12 +7,17 @@ import { SearchResponse } from "../models/SearchResponse";
 import { NavigationProp, ParamListBase } from "@react-navigation/native";
 
 import RecipeList from "../components/RecipeList";
+import SearchBarView from "../components/SearchBarView";
+import { Filter, getFilterApiKey } from "../models/Filter";
+import FilterBarView from "../components/FiIterBarView";
 
 interface SearchScreenProps {
   navigation: NavigationProp<ParamListBase>;
 }
 interface SearchScreenState {
   recipes: Recipe[];
+  searchText: string;
+  filter: string;
 }
 
 export default class SearchScreen extends Component<
@@ -21,7 +26,7 @@ export default class SearchScreen extends Component<
 > {
   constructor(props: SearchScreenProps) {
     super(props);
-    this.state = { recipes: [] };
+    this.state = { searchText: "", recipes: [], filter: Filter.all };
   }
 
   getRecipes(params: object) {
@@ -47,13 +52,22 @@ export default class SearchScreen extends Component<
     return { label, image, source, calories, totalWeight, totalTime, url };
   }
 
-  componentDidMount(): void {
-    this.getRecipes({ q: "chicken" });
-  }
-
   navigateToDetails(recipe: Recipe) {
     const { navigation } = this.props;
     navigation.navigate("DetailsScreen", { recipe });
+  }
+
+  handleSearch(text: string) {
+    this.getRecipes({ q: text });
+    this.setState({ searchText: text });
+  }
+
+  handleFilter(filter: string) {
+    this.getRecipes({
+      q: this.state.searchText,
+      ...getFilterApiKey(filter as Filter),
+    });
+    this.setState({ filter });
   }
 
   renderRecipeList() {
@@ -68,15 +82,27 @@ export default class SearchScreen extends Component<
   }
 
   render() {
-    return <View style={styles.container}>{this.renderRecipeList()}</View>;
+    return (
+      <SafeAreaView style={styles.container}>
+        <SearchBarView
+          text={this.state.searchText}
+          didSearch={(text) => this.handleSearch(text)}
+        />
+        <FilterBarView
+          filter={this.state.filter}
+          didFilter={(text) => this.handleFilter(text)}
+        />
+        {this.renderRecipeList()}
+      </SafeAreaView>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "stretch",
+    justifyContent: "flex-start",
     backgroundColor: "#abf",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
